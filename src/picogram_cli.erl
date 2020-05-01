c1af55f32f92e6b86f527042e3f213e140f415f2
Aaron Sutton <aaron@aaronjsutton.com>
2020-05-01 15:23:14 -0400

    Tar and make steps

--- src/picogram_cli.erl
+++ src/picogram_cli.erl
@@ -3,7 +3,7 @@
 
 -export([main/2]).
 
--export([print_version/1, connect/1]).
+-export([print_version/1, get_mix_version/1, make_tar/1, connect/1]).
 
 main(Steps, Args) ->
   lists:foldl(fun (Step, Ctx) ->
@@ -19,6 +19,24 @@ print_version(Ctx) ->
   io:format("=== picogram v~s~n", [Vsn]),
   Ctx.
 
+get_mix_version(Ctx) ->
+  Root = dict:fetch(release_root, Ctx),
+  RawVsn = os:cmd(io_lib:format("~s/bin/~s version", [Root, filename:basename(Root)])),
+  Vsn = string:trim(string:replace(RawVsn, " ", "-")),
+  dict:store(vsn, Vsn, Ctx).
+
+make_tar(Ctx) ->
+  Root = dict:fetch(release_root, Ctx),
+  Vsn = dict:fetch(vsn, Ctx),
+  io:format("=== Compressing release ~s...~n", [Root]),
+  {ok, Filenames} = file:list_dir(Root),
+  TarList = lists:map(fun (F) -> {F, filename:absname_join(Root, F)} end,
+                      Filenames),
+  Tar = filename:join(Root, io_lib:format("~s.tar.gz", [Vsn])),
+  erl_tar:create(Tar, TarList, [compress]),
+  io:format("=== Output release tar ~s~n", [Tar]),
+  dict:store(tar_path, Tar, Ctx).
+
 connect(Ctx) ->
   Host = dict:fetch(host, Ctx),
   Port = dict:fetch(port, Ctx),
@@ -29,3 +47,6 @@ connect(Ctx) ->
   {ok, Result} = picogram_rel_client:command(Conn, 0, 1000),
   io:format("\r=== Getting rel_server status... ~s~n", [Result]),
   dict:store(conn, Conn, Ctx).
+
+transfer_release(Ctx) -> Ctx.
+
--- src/picogram_rel_client.erl
+++ src/picogram_rel_client.erl
@@ -1,7 +1,7 @@
 %% SSH Interface with a remote release server.
 -module(picogram_rel_client).
 
--export([connect/3, command/3, command/4]).
+-export([connect/3, command/3, command/4, transfer/2]).
 
 connect(Host, Port, Options) ->
   ssh:connect(Host, Port, Options).
@@ -9,6 +9,7 @@ connect(Host, Port, Options) ->
 command(Conn, Command, Timeout) ->
   command(Conn, Command, "", Timeout).
 
+%% Execute a protocol defined command on the server
 command(Conn, Command, Data, Timeout) ->
   {ok, Session} = ssh_connection:session_channel(Conn, Timeout),
   success = ssh_connection:subsystem(Conn, Session, "rel_sshd", Timeout),
@@ -25,3 +26,9 @@ command(Conn, Command, Data, Timeout) ->
   end.
 
 header(Command) -> <<13:4, Command:4>>.
+
+transfer(Conn, File) ->
+  {ok, Channel} = ssh_sftp:start_channel(Conn),
+  {ok, Data} = file:read_file(File),
+  ok = ssh_sftp:write_file(Channel, filename:basename(File), []).
+
