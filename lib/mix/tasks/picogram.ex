28ca39fdaf51e465f0ffcebce257bf6ef0497e58
Aaron Sutton <aaron@aaronjsutton.com>
2020-05-01 20:21:20 -0400

    Update mix

--- lib/mix/tasks/picogram.ex
+++ lib/mix/tasks/picogram.ex
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Picogram do
   use Mix.Task
 
-  @shortdoc "Picogram mix interface"
+  @shortdoc "Picogram mix interface for Phoenix projects"
 
   @moduledoc """
     Deploy a release.
@@ -10,10 +10,10 @@ defmodule Mix.Tasks.Picogram do
   @doc false
   def run(args) do
     case args do
-      [] -> 
+      [host, port, user_dir, rel_root] -> 
         :application.ensure_all_started(:picogram)
         :application.ensure_all_started(:ssh)
-        :picogram_cli.main([:print_version, :connect], [{:host, 'localhost'}, {:port, 4000}, {:user_dir, 'priv/user'}])
+        :picogram_cli.main([{:host, to_charlist(host)}, {:port, port}, {:user_dir, to_charlist(user_dir)}, {:release_root, rel_root}, {:install_strategy, 'phoenix'])
     end
   end
 end
